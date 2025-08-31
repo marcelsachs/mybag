4003de60c68e78e23bb7ef4f870730f53b689a57
marcelsachs <sachsmarcel@proton.me>
2025-08-31 22:04:56 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,9 @@ echo "FONT=ter-124b" >> /etc/vconsole.conf
 echo "GRUB_FONT=/usr/share/grub/ter-u32b.pf2" >> /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
+cp .bashrc /sachs/.bashrc
+cp .vimrc /sachs/.vimrc
+
 mkdir -p /sachs/.config/i3status
 mkdir -p /sachs/.config/foot
 mkdir -p /sachs/.config/sway
