a6dd4cd5afeb604224c833c6cf033813c2bcac63
marcelsachs <sachsmarcel@proton.me>
2025-09-10 14:14:20 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,10 +4,6 @@ set -e
 
 sudo timedatectl set-ntp true
 
-sudo bash -c 'echo "FONT=ter-124b" >> /etc/vconsole.conf'
-sudo bash -c 'echo "GRUB_FONT=/usr/share/grub/ter-u32b.pf2" >> /etc/default/grub'
-sudo grub-mkconfig -o /boot/grub/grub.cfg
-
 cp .bashrc "$HOME/.bashrc"
 cp .vimrc "$HOME/.vimrc" 
 cp .gitconfig "$HOME/.gitconfig"
