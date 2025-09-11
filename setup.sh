3d894494b62a124d4063f90f1e70e4d84d80c91f
marcelsachs <sachsmarcel@proton.me>
2025-09-11 22:48:32 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -81,8 +81,8 @@ mkdir -p "$HOME/.config/i3status"
 mkdir -p "$HOME/.config/foot"
 mkdir -p "$HOME/.config/sway"
 
-cp $HOME/mybag/.config/foot/foot.ini "$HOME/.config/foot/"
-cp $HOME/mybag/.config/i3status/config "$HOME/.config/i3status/"
-cp $HOME/mybag/.config/sway/config "$HOME/.config/sway/"
+cp "$HOME/mybag/.config/foot/foot.ini" "$HOME/.config/foot/"
+cp "$HOME/mybag/.config/i3status/config" "$HOME/.config/i3status/"
+cp "$HOME/mybag/.config/sway/config" "$HOME/.config/sway/"
 
 echo "Setup OK! Get to work now."
